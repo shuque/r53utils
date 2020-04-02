f1841f0784b1164818927b93ba411913f5193715
Shumon Huque <shuque@gmail.com>
2020-04-01 23:23:47 -0400

    Cleanup code.

--- r53_listzones.py
+++ r53_listzones.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 #
 
-import os, sys, boto3, pprint
+"""
+Print all hosted Route53 zones in the current account.
+
+"""
+
+import boto3
+
 
 # Can't be more than 100 apparently.
 MAXITEMS = '100'
 
 
-def print_zones(response):
-    for zone in response['HostedZones']:
+def print_zones(hostedzones_list):
+    """Print zone info: name, zoneid, type, rrset count"""
+
+    for zone in hostedzones_list:
         print("{} {} {} {}".format(
             zone['Name'],
             zone['Id'],
@@ -16,18 +24,25 @@ def print_zones(response):
             zone['ResourceRecordSetCount']))
 
 
+def status(http_response):
+    """return response HTTP status code"""
+
+    return http_response['ResponseMetadata']['HTTPStatusCode']
+
+
 if __name__ == '__main__':
 
     client = boto3.client('route53')
 
-    response = client.list_hosted_zones_by_name(MaxItems=MAXITEMS)
-    print_zones(response)
-    
-    truncated = response['IsTruncated']
-    while truncated:
-        response = client.list_hosted_zones_by_name(
-            HostedZoneId=response['NextHostedZoneId'],
-            DNSName=response['NextDNSName'],
-            MaxItems=MAXITEMS)
-        print_zones(response)
-        truncated = response['IsTruncated']
+    kwargs = dict(MaxItems=MAXITEMS)
+    while True:
+        response = client.list_hosted_zones_by_name(**kwargs)
+        if status(response) != 200:
+            raise Exception("list_hosted_zones_by_name() error: {}".format(
+                response))
+        print_zones(response['HostedZones'])
+        if response['IsTruncated']:
+            kwargs['DNSName'] = response['NextDNSName']
+            kwargs['HostedZoneId'] = response['NextHostedZoneId']
+        else:
+            break
