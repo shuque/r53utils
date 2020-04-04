f328d7c2df4780a0d6e60bfe0694c8103bba3fbf
Shumon Huque <shuque@gmail.com>
2020-04-04 15:53:53 -0400

    redo with lib

--- testdnsanswer.py
+++ testdnsanswer.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
 #
 
-import os, sys, boto3, pprint
+import sys
+import r53utils
 
 
 if __name__ == '__main__':
 
-    client = boto3.client('route53')
     zoneid, qname, qtype = sys.argv[1:]
-
-    response = client.test_dns_answer(
-        HostedZoneId=zoneid,
-        RecordName=qname,
-        RecordType=qtype)
-    pprint.pprint(response)
-
+    client = r53utils.get_client()
+    r53utils.test_dns_answer(client, zoneid, qname, qtype)
